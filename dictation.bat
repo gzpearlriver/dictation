92f69322de98d4ca0b2ddcf1d2b19b423be30519
gzpearlriver <frank@139.com>
2019-01-22 21:24:28 +0800

    modify dictation.bat

--- dictation.bat
+++ dictation.bat
@@ -1,6 +1,8 @@
 echo off
-path=%path%;C:\Python36;C:\Python36\Lib;C:\Python36\Scripts;D:\gdriver\code\dictation;
-python D:\gdriver\code\dictation\student.py
+path=%path%;C:\Python;C:\Python\Lib;C:\Python\Scripts;D:\code\dictation;C:\Program Files\Git\cmd;
+cd D:\code\dictation
+git pull
+python student.py
 echo on
 echo "finished!"
 pause
