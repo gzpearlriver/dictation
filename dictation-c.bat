0cc19df203249f04ecab2af4d8f5197b6a414a5b
gzpearlriver <frank@139.com>
2020-12-30 09:17:03 +0800

    -c

--- dictation-c.bat
+++ dictation-c.bat
@@ -2,7 +2,7 @@ echo off
 path=%path%;C:\Python;C:\Python\Lib;C:\Python\Scripts;C:\Program Files\Git\cmd;
 cd C:\code\dictation
 git pull
-python student.py
+c:\Python\python.exe student.py
 echo on
 echo "finished!"
 pause
