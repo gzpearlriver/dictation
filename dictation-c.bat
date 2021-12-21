f3008e43106d8ce85fcec8143f192c88116f3a8f
gzpearlriver <frank@139.com>
2021-12-21 23:05:56 +0800

    change sql server

--- dictation-c.bat
+++ dictation-c.bat
@@ -2,7 +2,7 @@ echo off
 path=%path%;C:\Python;C:\Python\Lib;C:\Python\Scripts;C:\Program Files\Git\cmd;
 cd C:\code\dictation
 git pull
-c:\Python\python.exe student.py
+python student.py
 echo on
 echo "finished!"
 pause
